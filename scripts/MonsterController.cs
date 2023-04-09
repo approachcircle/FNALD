bed111c2d44641d70f1baac13696c9efab6026b4
approachcircle <osc-mcd@protonmail.com>
2023-04-09 03:32:32 +0100

    add correct behavior for monster movement

--- scripts/MonsterController.cs
+++ scripts/MonsterController.cs
@@ -1,24 +1,36 @@
 using Godot;
-using System.Collections.Generic;
 
 public partial class MonsterController : Node
 {
-    public static Monster[] Monsters { get; set; } = {
-		new Cameron()
+	[Signal]
+	public delegate void MonsterMovedEventHandler(string monsterName);
+
+	private static readonly Monster[] monsters = {
+		new Cameron(),
+		new Harry()
 	};
+
 	public override void _Ready()
 	{
-
+		GetNode<Timer>("Movement").Timeout += RollMonsters;
+		MonsterMoved += (monsterName) => { GetNode<Label>("MoveList").Text += $"\n{monsterName}"; };
 	}
 
 	public override void _Process(double delta)
 	{
-		if (Global.MonstersEnabled)
+
+	}
+
+	private void RollMonsters()
+	{
+		if (!Global.MonstersEnabled) return;
+		foreach (Monster monster in monsters)
 		{
-			foreach (Monster monster in Monsters)
-				if (monster.Roll())
-					// TODO: monster movement behaviour
-					monster.Room = monster.Destination;
+			if (monster.Roll())
+			{
+				EmitSignal(nameof(MonsterMoved), monster.Name);
+				monster.Move(monster.Direction);
+			}
 		}
 	}
 }
