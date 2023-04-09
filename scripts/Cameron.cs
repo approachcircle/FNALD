443b4c7215a66ec20c24d8fbf9737622163e5321
approachcircle <osc-mcd@protonmail.com>
2023-04-09 03:29:09 +0100

    conform cameron to new monster structure

--- scripts/Cameron.cs
+++ scripts/Cameron.cs
@@ -1,16 +1,4 @@
-﻿using System;
-
-public class Cameron : Monster
+﻿public class Cameron : Monster
 {
-    new public string Name { get; set; } = "Cameron";
-    new public int Hostility { get; set; } = 0;
-    public override bool Roll()
-    {
-        return false;
-    }
-
-    public override void SetDestination()
-    {
-        throw new NotImplementedException();
-    }
+    public override string Name { get; protected set; } = "Cameron";
 }
