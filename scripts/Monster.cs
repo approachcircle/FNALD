a97273ecb90641a18c97466966fed970636b3477
approachcircle <osc-mcd@protonmail.com>
2023-04-10 03:29:16 +0100

    scale difficulty with night number

--- scripts/Monster.cs
+++ scripts/Monster.cs
@@ -1,5 +1,4 @@
-﻿using Godot;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 
@@ -7,29 +6,43 @@ public abstract class Monster
 {
     public abstract string Name { get; protected set; }
 
-    public virtual Direction Direction { get; protected set; } = Direction.Forward;
+    public virtual Direction Direction { get; protected set; }
 
     public virtual Room Room { get; protected set; } = Room.Stage;
 
-    public int Hostility { get; protected set; } = 300;
+    public virtual int BaseHostility { get; protected set; } = 300;
 
-    public virtual int Neutrality { get; protected set; } = 500;
+    public virtual int DynamicHostility { get; protected set; }
+
+    public virtual int BaseNeutrality { get; protected set; } = 600;
+
+    public virtual int DynamicNeutrality { get; protected set; }
+
+    public Monster()
+    {
+        CalculateDifficulty();
+    }
 
     public virtual bool Roll()
     {
-        if (new Random().Next(1, Hostility) == Hostility / 2)
+        if (Global.AngerRate != 1) DynamicHostility -= Global.AngerRate * 2;
+        if (Global.AngerRate != 1) DynamicNeutrality += Global.AngerRate * 2;
+        if (new Random().Next(1, DynamicHostility) == DynamicHostility / 2)
         {
-            if (Room == Room.Office) { return false; }
+            if (Room == Room.Office) return false;
             Direction = Direction.Forward;
             return true;
         }
-        else if (new Random().Next(1, Neutrality) == Neutrality / 2)
+        else if (new Random().Next(1, DynamicNeutrality) == DynamicNeutrality / 2)
         {
             if (Room == Room.Stage) return false;
+            if (Room == Room.B1) return false;
             Direction = Direction.Backward;
             return true;
-        } else
+        }
+        else
         {
+            CalculateDifficulty();
             return false;
         }
     }
@@ -38,12 +51,14 @@ public abstract class Monster
     {
         List<Room> rooms = Enum.GetValues<Room>().ToList();
         if (direction == Direction.Forward)
-        {
             Room = rooms[rooms.IndexOf(Room) + 1];
-        }
         else
-        {
             Room = rooms[rooms.IndexOf(Room) - 1];
-        }
+    }
+
+    protected virtual void CalculateDifficulty()
+    {
+        DynamicHostility = BaseHostility - (55 * (int)Global.GetNight());
+        DynamicNeutrality = BaseNeutrality + (20 * (int)Global.GetNight());
     }
 }
