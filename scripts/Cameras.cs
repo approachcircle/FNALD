9a763a2fcc35a92538815df2ca481b161510dd9f
approachcircle <osc-mcd@protonmail.com>
2023-04-10 02:47:47 +0100

    cleanup cameras

--- scripts/Cameras.cs
+++ scripts/Cameras.cs
@@ -3,24 +3,23 @@ using System;
 
 public partial class Cameras : Control
 {
-	bool light_visible = true;
+	bool lightVisible = true;
 	public override void _Ready()
 	{
-		GetNode<Timer>("Flash").Timeout += Flash;
+		GetNode<Timer>("Flash").Timeout += () => { lightVisible = !lightVisible; };
 	}
 
 	public override void _Process(double delta)
 	{
-		Sprite2D recording = GetNode<Sprite2D>("Recording");
-		if (light_visible)
-		{
-			Global.ModulateNodeAlpha(recording, 1);
-		}
-		else
-		{
-			Global.ModulateNodeAlpha(recording, 0);
-		}
+
 	}
 
-	private void Flash() => light_visible = !light_visible;
+	private void FlashLight()
+	{
+        Sprite2D recording = GetNode<Sprite2D>("Recording");
+        if (light_visible)
+            Global.ModulateNodeAlpha(recording, 1);
+        else
+            Global.ModulateNodeAlpha(recording, 0);
+    }
 }
