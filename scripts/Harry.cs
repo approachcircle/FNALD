eb6287d58598f0801a235175f2a84b84cd28918f
approachcircle <osc-mcd@protonmail.com>
2023-04-13 00:02:18 +0100

    fix formatting

--- scripts/Harry.cs
+++ scripts/Harry.cs
@@ -1,5 +1,5 @@
-﻿public class Harry : Monster
+public class Harry : Monster
 {
-    public override string Name { get; protected set; } = "Harry";
+	public override string Name { get; protected set; } = "Harry";
 }
 
