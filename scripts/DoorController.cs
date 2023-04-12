73a8e7b8f0f959c838792cab4253b89fdb95b102
approachcircle <osc-mcd@protonmail.com>
2023-04-12 02:28:22 +0100

    add visibility toggles for doors

--- scripts/DoorController.cs
+++ scripts/DoorController.cs
@@ -3,26 +3,34 @@ using System;
 
 public partial class DoorController : Node
 {
-	ColorRect midDoorHitbox;
-	ColorRect leftDoorHitbox;
+	TextureButton midDoor;
+	CanvasGroup leftDoor;
 	public override void _Ready()
 	{
-		GetNode<TextureButton>("MidDoor").Pressed += MidDoorClicked;
-		GetNode<Button>("LeftDoor/Clickable").Pressed += LeftDoorClicked;
+		midDoor = GetNode<TextureButton>("MidDoor");
+		leftDoor = GetNode<CanvasGroup>("LeftDoor");
+		midDoor.Pressed += MidDoorClicked;
+		leftDoor.GetNode<Button>("Clickable").Pressed += LeftDoorClicked;
 	}
 
 	public override void _Process(double delta)
 	{
-		
+		UpdateDoorVisuals();
 	}
 
 	private void MidDoorClicked()
 	{
-
+		Global.MidDoorClosed = !Global.MidDoorClosed;
 	}
 
 	private void LeftDoorClicked()
 	{
+		Global.LeftDoorClosed = !Global.LeftDoorClosed;
+	}
 
+	private void UpdateDoorVisuals()
+	{
+		leftDoor.Visible = Global.LeftDoorClosed;
+		midDoor.Visible = !Global.MidDoorClosed;
 	}
 }
