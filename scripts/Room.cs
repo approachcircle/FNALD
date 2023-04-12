64ef9e4522e742d6218215c9bb125c80c769a0da
approachcircle <osc-mcd@protonmail.com>
2023-04-13 00:23:22 +0100

    add two new rooms, and method to get rooms as list

--- scripts/Room.cs
+++ scripts/Room.cs
@@ -1,10 +1,22 @@
-﻿public enum Room
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public enum Room
 {
     Stage,
     B1,
     B2,
     A1,
     A2,
+    C1,
+    C2,
     Office
 }
-
+public static class Extensions
+{
+    public static List<Room> GetRooms(this Room room)
+    {
+        return Enum.GetValues<Room>().ToList();
+    }
+}
